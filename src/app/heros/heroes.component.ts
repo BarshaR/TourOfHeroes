dbee546a83c8e91a1a6b6d70802df59c1b50603e
Richard Barsha <barsha.richard@gmail.com>
2018-12-22 12:46:24 +1100

    Separated components to use the router instead

--- src/app/heros/heroes.component.ts
+++ src/app/heros/heroes.component.ts
@@ -10,9 +10,7 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -20,12 +18,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
 }
